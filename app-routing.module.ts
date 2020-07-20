1dd81a9a5b3588f2e475afb59583e24d6e8b2f6d
jflorence98 <67752473+jflorence98@users.noreply.github.com>
2020-07-20 21:52:13 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = []; 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+  
+})
+export class AppRoutingModule { }
